a3f1b31b9c2aea981d25c3bdb81cb6b52a1c0960
dogfew <volodia.perepyolckin@gmail.com>
2023-11-20 02:03:47 +0300

    smallfix

--- prep_script.sh
+++ prep_script.sh
@@ -1,7 +1,7 @@
 
 ##download LjSpeech
 wget https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2 -o /dev/null
-mkdir data
+mkdir -p data
 tar -xvf LJSpeech-1.1.tar.bz2 >> /dev/null
 mv LJSpeech-1.1 data/LJSpeech-1.1
 
