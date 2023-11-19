19e8d9aee54f79a88847095195cedb0ca4541116
dogfew <volodia.perepyolckin@gmail.com>
2023-11-20 00:06:28 +0300

    Changes in preparation script

--- prep_script.sh
+++ prep_script.sh
@@ -13,9 +13,9 @@ gdown https://drive.google.com/u/0/uc?id=1WsibBTsuRg_SF2Z6L6NFRTT-NjEy1oTx
 mkdir -p waveglow/pretrained_model/
 mv waveglow_256channels_ljs_v2.pt waveglow/pretrained_model/waveglow_256channels.pt
 
-gdown https://drive.google.com/u/0/uc?id=1cJKJTmYd905a-9GFoo5gKjzhKjUVj83j
-tar -xvf mel.tar.gz
-echo $(ls mels | wc -l)
+#gdown https://drive.google.com/u/0/uc?id=1cJKJTmYd905a-9GFoo5gKjzhKjUVj83j
+#tar -xvf mel.tar.gz
+#echo $(ls mels | wc -l)
 
 gdown 1Vtqk3tE3hcp2cZI76HCipD18jLStVBeP
 unzip MFA.zip
